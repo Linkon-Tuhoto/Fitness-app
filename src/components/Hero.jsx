1fcd0aca60f1eda5b296eb3f37d2bed208ec4504
Linkon-Tuhoto <linkonngere@gmail.com>
2026-05-14 17:21:15 +0300

    Hero structure

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,54 @@ function Hero() {
                 </p>
             </div>
         </div>
-        <div></div>
+        {/*Hero image*/}
+        <div>
+        <div style={{ backgroundImage: 'url(${heroImg})'}}>
+            <p>Focus</p>
+            <h1>
+                DISCIPLINE
+                <br />
+                BUILDS POWER
+            </h1>
+            <p>
+                Stay focused. Stay consistent. See results.
+            </p>
+            <div>
+                <button>Start Workout</button>
+            </div>
+        </div>
+        {/*Today's Goal*/}
+        <div>
+            <h2>Today's Goal</h2>
+            <div>
+                <div>
+                    <h2>
+                        450
+                    </h2>
+                    <p>
+                        Calories
+                    </p>
+                </div>
+                <div>
+                    <h2>
+                        32 
+                    </h2>
+                    <p>
+                        Minutes
+                    </p>
+                </div>
+                <div>
+                    <h2>
+                        5
+                    </h2>
+                    <p>
+                        Workouts
+                    </p>
+                </div>
+            </div>
+        </div>
+        </div>
+
     </div>
   )
 }
