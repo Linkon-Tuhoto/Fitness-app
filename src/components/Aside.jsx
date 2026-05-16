ca506c00c0d418f17e6137232434a137a6d38bea
Linkon-Tuhoto <linkonngere@gmail.com>
2026-05-16 08:07:01 +0300

    sidebar structure layout

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -2,7 +2,61 @@ import React from 'react'
 
 function Aside() {
   return (
-    <div>Aside</div>
+    <div>
+      <aside>
+        <h2>
+          FitPulse
+        </h2>
+
+        <div>
+          <button>
+            Dashboard
+          </button>
+
+          <button>
+            Workouts
+          </button>
+
+          <button>
+            Nutrition
+          </button>
+
+          <button>
+            Progress
+          </button>
+
+          <button>
+            Calendar
+          </button>
+
+          <button>
+            Music
+          </button>
+
+          <button>
+            Settings
+          </button>
+        </div>
+
+        <div>
+          <div>
+            <img src="" alt="" />
+            <div>
+              <h2>James King'ori</h2>
+              <p>Level 18</p>
+            </div>
+          </div>
+          <div>
+            <div></div>
+            <div>
+              <h2>
+                70%
+              </h2>
+            </div>
+          </div>
+        </div>
+      </aside>
+    </div>
   )
 }
 
